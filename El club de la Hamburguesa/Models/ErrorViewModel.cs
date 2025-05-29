a1b1068018d2253ed12682dafa2412a61e618932
LucilaMicheloud <micheloudlucila@gmail.com>
2025-05-29 10:18:11 -0300

    Agregar archivos de proyecto.

---
+++ El club de la Hamburguesa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace El_club_de_la_Hamburguesa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
