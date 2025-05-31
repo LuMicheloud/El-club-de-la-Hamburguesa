8800b2a385e7be62323656bd3480de20c776ea0e
LucilaMicheloud <micheloudlucila@gmail.com>
2025-05-31 12:13:54 -0300

    Cree la clase Hamburguesa con el constructor

---
+++ El club de la Hamburguesa/Views/Home/Class2.cs
@@ -0,0 +1,20 @@
+﻿namespace El_club_de_la_Hamburguesa.Views.Home
+{
+    public class Hamburguesa{
+        private int id;
+        private String nombre;
+        private String descripcion;
+        private double precio;
+
+        public Hamburguesa(int id, string nombre, string descripcion, double precio)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+        }
+
+
+
+    }
+}
