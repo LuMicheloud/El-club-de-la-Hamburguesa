cce2b30b9c9dbf3b70c5045361c84c4bfd6218b2
LucilaMicheloud <micheloudlucila@gmail.com>
2025-05-31 12:00:58 -0300

    Agregue la clase Tarjeta

---
+++ El club de la Hamburguesa/Views/Home/Class.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace El_club_de_la_Hamburguesa.Views.Home
+{
+    public class Usuario {
+        private int id;
+        private String name;
+        private String email;
+        private String contrasenia;
+        private Boolean descuentoBienvenidaAplicado;
+
+        public Usuario(int id, String nombre, String email, String contrasenia)
+        {
+
+            this.id = id;
+            this.email = email;
+            this.contrasenia = contrasenia;
+
+        }
+        public int getId() { return id; 
+        }
+        public String getName() { return name;
+        }
+        public String getEmail() { return email; 
+        }
+
+
+        public Boolean verificarContrasenia (String contrasenia) {
+
+            return !string.IsNullOrEmpty(contrasenia);
+
+        } 
+
+
+
+    }
+}
---
+++ El club de la Hamburguesa/Views/Home/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace El_club_de_la_Hamburguesa.Views.Home
+{
+    public class Tarjeta {
+        private String numero;
+        private String titular;
+        private String vencimiento;
+        private String codigoDeSeguridad;
+
+        public Tarjeta(string numero, string titular, string vencimiento, string codigoDeSeguridad)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.vencimiento = vencimiento;
+            this.codigoDeSeguridad = codigoDeSeguridad;
+        }
+
+        public Boolean EsValida(string numero) {
+        
+        } //Return Tarjeta.numero;//
+    }
+}
