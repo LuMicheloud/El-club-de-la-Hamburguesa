801256099f622b6e65a140445e1d765be4d8195b
LucilaMicheloud <micheloudlucila@gmail.com>
2025-05-31 19:14:11 -0300

    Cree interfaz TICKET

---
+++ El club de la Hamburguesa/Iticket.cs
@@ -0,0 +1,12 @@
+﻿using El_club_de_la_Hamburguesa.Views.Home;
+
+namespace El_club_de_la_Hamburguesa
+{
+    public interface Iticket
+    {
+        public void imprimir() {
+        }
+
+       //public Iticket ticket(Pedido) { }
+    }
+}
